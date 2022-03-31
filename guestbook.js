6f610ae5bc3c140c46637ecb552427b0f3533cdb
heyheywastaken <100477869+heyheywastaken@users.noreply.github.com>
2022-04-01 00:49:05 +0530

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://f63f46b7.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
